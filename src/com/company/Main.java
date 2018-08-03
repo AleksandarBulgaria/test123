f8a561bc8d50116d16d9f17de3b6217302152496
Aleksandar <Aleksandar@EPBGSOFW0145.budapest.epam.com>
2018-08-03 13:11:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your name: ");
+        String name = reader.readLine();
+        System.out.println("Your name is: " + name);
+    }
+}
