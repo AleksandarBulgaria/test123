4ddf4c648ce1d3f35b0990126fe9acb592f89055
Aleksandar <aleksanderst@abv.bg>
2018-08-03 15:48:37 +0300

    local commit

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.print("Enter your name: ");
         String name = reader.readLine();
         System.out.println("Your name is: " + name);
+        int n = reader.read();
+
     }
 
     public static int add(int a, int b) {
