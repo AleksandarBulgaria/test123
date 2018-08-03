3449f2bd55fbed9571591c7a178d66898efb9a2f
Aleksandar <aleksanderst@abv.bg>
2018-08-03 17:50:28 +0300

    local commit

---
+++ src/main/java/com/company/JavaOutputFormatting.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s = scan.next();
+            int x = scan.nextInt();
+            System.out.printf("%-14s %03d\n", s, x);
+        }
+        System.out.println("================================");
+    }
+}
